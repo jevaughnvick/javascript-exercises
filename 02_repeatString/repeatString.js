69213440054ab462b1f50d9cadd4429f264c1963
Jevaughn Vickers <114950498+jevaughnvick@users.noreply.github.com>
2024-06-12 23:34:07 -0400

    pass 7 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    if(num < 0) return "ERROR";
+    
+    let final = "";
+    for(let i = 0; i < num; i++){
+        final += string;
+    }
+
+    return final
 };
+
+// console.log(repeatString("bad", 2))
 
 // Do not edit below this line
 module.exports = repeatString;
