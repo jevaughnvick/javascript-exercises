1f8378d8f767ff9283e6fa372e7a5f7e25a8bc6d
Jevaughn Vickers <114950498+jevaughnvick@users.noreply.github.com>
2024-06-13 21:48:02 -0400

    pass all 4 tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,22 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let array = string.split("");
+    let reversedArray = [];
+    let arrayLength = array.length;
+    // console.log(array)
+    for(let i = 0; i < arrayLength; i++){
+
+        let letter = array.pop();
+        reversedArray.push(letter);
+    }
+
+    return(reversedArray.join(""))
 };
+
+// reverseString("hey")
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+// convert string to array
