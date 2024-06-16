a32345668acce7668ef05867fc2aa998ecc489d9
Jevaughn Vickers <114950498+jevaughnvick@users.noreply.github.com>
2024-06-15 23:58:00 -0400

    pass all 7 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...targets) {
+
+    let newArray = [];
+
+    nextElem:
+    for(let elem of array){
+
+        for(let target of targets){
+
+            if(elem !== target) continue;
+            continue nextElem;
+        }
+
+        newArray.push(elem);
+    }
+
+    return newArray;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
