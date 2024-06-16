77218e999533c8a911f805201434a6c4d5897904
Jevaughn Vickers <114950498+jevaughnvick@users.noreply.github.com>
2024-06-16 08:35:46 -0400

    pass all 6 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    if(start < 0 || end < 0) return "ERROR";
+    if(typeof start !== "number" || typeof end !== "number") return "ERROR";
+
+    let total = 0;
+    for(let i = start; start < end ? i <= end : i >= end; start < end? i++ : i--){
+
+        total += i;
+    }
+    return total;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
