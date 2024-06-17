ac3b460ff314530cc237062f0855582120a8ff50
Jevaughn Vickers <114950498+jevaughnvick@users.noreply.github.com>
2024-06-16 22:36:36 -0400

    pass all 16 tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+
+	return numOne + numTwo;
 };
 
-const subtract = function() {
+const subtract = function(numOne, numTwo) {
 	
+	return numOne - numTwo;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+
+	let sum = 0;
+
+	for(let elem of array){
+
+		sum += elem;
+	}
+
+	return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
 
+	let product = 1;
+
+	for(let elem of array){
+
+		product *= elem;
+	}
+
+	return product;
 };
 
-const power = function() {
-	
+const power = function(numOne, numTwo) {
+
+	return numOne ** numTwo;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+
+	let factorial = 1;
+
+	for(let i = 1; i <= num; i++){
+
+		factorial *= i;
+	}
+
+	return factorial;
 };
 
 // Do not edit below this line
