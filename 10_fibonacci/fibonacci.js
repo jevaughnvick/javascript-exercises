2152f45462aa805a7bff9d23b813fde710f27718
Jevaughn Vickers <114950498+jevaughnvick@users.noreply.github.com>
2024-08-04 12:55:31 -0700

    pass all 11 tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
 
+    if(member == 0) return 0;
+    if(member < 0) return "OOPS";
+    const fibMembers = [1, 1];
+
+    for(let i = 1; fibMembers.length <= (member - 1); i++){
+
+        let precede = i - 1;
+        fibMembers.push(fibMembers[i] + fibMembers[precede]);
+    }
+    return fibMembers[fibMembers.length - 1];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
