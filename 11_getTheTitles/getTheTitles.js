8bed7da593f298ec062d88b829851bf8924510e5
Jevaughn Vickers <114950498+jevaughnvick@users.noreply.github.com>
2024-08-06 12:13:34 -0700

    pass 1 test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
 
+    return array.map(object => object.title)
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
